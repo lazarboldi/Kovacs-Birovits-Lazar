a50c8694aa706a30732ad15d982d006fada1a150
Levente Szomor <59541573+SzmrLevee@users.noreply.github.com>
2024-05-29 00:33:06 +0200

    Teljes projekt feltöltése

---
+++ Labiko/ClassLib/Kihivas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadgeSystem
+{
+    public class Kihivas
+    {
+        public string Feladatmegnevezes { get; init; }
+        public string Feladat { get; init; }
+        public string Teljesites { get; set; }
+
+        public Kihivas(string adatsor)
+        {
+            string[] adatelemek = adatsor.Split(";");
+            Feladatmegnevezes = adatelemek[0];
+            Feladat = adatelemek[1];
+            Teljesites = adatelemek[2];
+        }
+    }
+}
